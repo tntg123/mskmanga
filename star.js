faa6356c02acc4b14fee45a7ad48a4f62f307d6c
TNV2k5 <92280434+tntg123@users.noreply.github.com>
2022-05-16 16:47:29 +0700

    Create star.js

---
+++ star.js
@@ -0,0 +1,34 @@
+    
+    // Async Rating-Widget initialization.
+    window.RW_Async_Init = function(){
+                
+        RW.init({
+            huid: "483916",
+            uid: "172e403292ca6fb1f28f899e5de040f6",
+            source: "website",
+            options: {
+                "advanced": {
+                    "font": {
+                        "type": "tahoma"
+                    }
+                },
+                "size": "medium",
+                "lng": "vi",
+                "style": "oxygen",
+                "isDummy": false
+            } 
+        });
+        RW.render();
+    };
+        // Append Rating-Widget JavaScript library.
+    var rw, s = d.getElementsByTagName(e)[0], id = "rw-js",
+        l = d.location, ck = "Y" + t.getFullYear() + 
+        "M" + t.getMonth() + "D" + t.getDate(), p = l.protocol,
+        f = ((l.search.indexOf("DBG=") > -1) ? "" : ".min"),
+        a = ("https:" == p ? "secure." + m + "js/" : "js." + m);
+    if (d.getElementById(id)) return;              
+    rw = d.createElement(e);
+    rw.id = id; rw.async = true; rw.type = "text/javascript";
+    rw.src = p + "//" + a + "external" + f + ".js?ck=" + ck;
+    s.parentNode.insertBefore(rw, s);
+    }(document, new Date(), "script", "rating-widget.com/"))
